6a67e6ef8bd1a48f735a5abc342507f67c6af1ef
KunHwan Ahn <kunhwon@gmail.com>
2015-06-04 18:32:52 +0900

    Use my first library in Go lang

--- hello.go
+++ hello.go
@@ -1,7 +1,10 @@
 package main
 
-import "fmt"
+import (
+    "fmt"
+    "github.com/KunHwanAhn/stringutil"
+)
 
 func main() {
-    fmt.Println("Hello, world\n")
+    fmt.Println(stringutil.Reverse("!oG ,olleH"))
 }
